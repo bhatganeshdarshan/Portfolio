489de9f099027aa29926b11dedadbbc8f5427d66
Ganeshdarshan <bhatganeshdarshan10@gmail.com>
2024-09-12 18:43:38 +0530

    skill cards updated

--- src/components/SkillBar.jsx
+++ src/components/SkillBar.jsx
@@ -1,27 +1,47 @@
-import React , {useEffect,useState} from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import '../styles/Skills.css';
 
+export default function SkillBar({ lang, perc, icon }) {
+  const [isVisible, setIsVisible] = useState(false);
+  const skillRef = useRef(null);
 
-export default function SkillBar({lang,perc}){
-    const [widths , setWidths] = useState(0);
-
-    useEffect(
-        ()=>{
-            requestAnimationFrame(()=>setWidths(perc));
-        },
-        [perc]
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setIsVisible(true);
+          observer.disconnect();
+        }
+      },
+      { threshold: 0.5 }
     );
 
-    return (
-        <div className='skill'>
-            <span className='skill-name'>{lang}</span>
-            <div className='skill-bar-outer'>
-                <div className='skill-bar-inner' style={{
-                    width : widths , 
-                    transition : 'width 1.5s ease-in-out',
-                }}>
-                </div>
-            </div>
+    if (skillRef.current) {
+      observer.observe(skillRef.current);
+    }
+
+    return () => {
+      if (skillRef.current) observer.disconnect();
+    };
+  }, []);
+
+  return (
+    <div className="skill" ref={skillRef}>
+      <div className='skill-icon-container'>
+        <div className="skill-icon">
+            <span dangerouslySetInnerHTML={{ __html: icon }} />
         </div>
-    );
+        <span className="skill-name">{lang}</span>
+      </div>
+      <div className="skill-bar-outer">
+        <div
+          className="skill-bar-inner"
+          style={{
+            width: isVisible ? perc : '0%',
+            transition: 'width 1.5s ease-in-out',
+          }}
+        ></div>
+      </div>
+    </div>
+  );
 }
